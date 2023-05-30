2379113570a6213fb88590b4c1a74d0658ce61d8
t.yaginuma <t.yaginuma@grooowth.co.jp>
2023-05-30 11:38:01 +0900

    update front実装

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+      rollupOptions: {
+        output: {
+          entryFileNames: "assets/entry.[hash].js",
+          assetFileNames: "assets/asset.[hash][extname]",
+        },
+      },
+    },
+  },
+});
