ab8544d94b0e724d9bb0761dee2eb061c967a41f
t.yaginuma <t.yaginuma@grooowth.co.jp>
2023-06-02 22:16:31 +0900

    update 画面実装

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,15 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "src/styles/index.scss";
+          `,
+        },
+      },
+    },
     build: {
       assetsInlineLimit: 0,
       rollupOptions: {
